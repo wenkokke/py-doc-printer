a1d5ae9af287486db627425c67edbcd06f2dfc28
Wen Kokke <wenkokke@users.noreply.github.com>
2022-07-20 23:06:18 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.md")) as f:
 
 setup(
     name="prettyprinter",
-    version="0.1.1",
+    version="0.1.2",
     maintainer="Wen Kokke",
     maintainer_email="me@wen.works",
     author="Wen Kokke",
