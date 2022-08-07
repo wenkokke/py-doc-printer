374ab8583e3378209b426a04483c59113a394431
Wen Kokke <wenkokke@users.noreply.github.com>
2022-08-07 17:15:31 +0100

    Bump version 0.12.0 -> 0.12.1

--- doc-source/conf.py
+++ doc-source/conf.py
@@ -26,7 +26,7 @@ copyright = "2022, Wen Kokke"
 author = "Wen Kokke"
 
 # The full version, including alpha/beta/rc tags
-release = "0.12.0"
+release = "0.12.1"
 
 
 # -- General configuration ---------------------------------------------------
